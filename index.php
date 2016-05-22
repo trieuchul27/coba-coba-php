a139c5858a377afba9502b4d2d71a9a0c7ed10db
trieuchul27 <trie.uchul96@gmail.com>
2016-05-22 21:27:27 +0700

    versi 1.0.0

---
+++ index.php
@@ -0,0 +1,39 @@
+
+ <!DOCTYPE html>
+<html>
+<body bgcolor="pink">
+
+    <h2><font face="Andalus" size=10>MY SCHOOL</font></h2>
+
+<?php
+echo "Welcome Praktikkum PHP!";
+?>
+<?php
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+
+<h4>sensitiv PHP </h4>
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $color . "<br>";
+echo "My boat is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+
+</body>
+</html> 
